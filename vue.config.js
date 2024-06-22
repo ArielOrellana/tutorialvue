9dddd26824039b744cceed86c3244eaf10d8a726
ArielOrellana <orellanaariel1998@gmail.com>
2024-06-22 13:09:07 -0300

    deploy fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/tutorialvue/',
+  outputDir:'dist',
+  assetsDir:'static'
 })
